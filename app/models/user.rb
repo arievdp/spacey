d6db98d1d18979cccb19d40cbfdb60117c904cf4
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-06-01 21:06:06 +1200

    ARCH: Configured Devise, added feilds to user model, commented out phone validation as it's throwing an error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-#   has_many :orders
-#   validates :first_name, :last_name, :user_name, :phone_number, presence: true
-#   validates :first_name, uniqueness: { scope: :last_name}
-#   validates :phone_number, format: { with: /^(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}$/ }
+  has_many :orders
+  validates :first_name, :last_name, :user_name, :phone_number, presence: true
+  validates :first_name, uniqueness: { scope: :last_name }
+  # validates :phone_number, format: { with: /^(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}$/ }
+  # error when validationg phone numer: The provided regular expression is using multiline anchors (^ or $), which may present a security risk. Did you mean to use \A and \z, or forgot to add the :multiline => true option? (ArgumentError)
 end
