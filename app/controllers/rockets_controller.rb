fa64ea934b896a2639679ded8f6c61fe5ccf1159
Lacy Zhang <lacy.zhang@hotmail.com>
2020-06-01 16:06:07 +1000

    todo rockets controller

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -1,2 +1,37 @@
 class RocketsController < ApplicationController
+  before_action :find_rocket
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_rocket
+    @rocket = Rocket.find(params[:id])
+  end
+
+  def review_params
+    # params.require(:review).permit(:content)
+  end
 end
