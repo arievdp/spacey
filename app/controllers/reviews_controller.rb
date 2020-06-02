206966fb91dd8f66bcc04f9ffa55434555b7bb0e
Jason Sim <jschinsin5@hotmail.com>
2020-06-02 11:10:18 +1000

    done reviews-controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @rocket = Rocket.find(params[:rocket_id])
+    @review = Review.new
+  end
+
+  def create
+    @rocket = Rocket.find(params[:rocket_id])
+    @review = Review.new(review_params)
+    @review.rocket = @rocket
+    if @review.save
+      redirect_to rocket_path(@rocket)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
