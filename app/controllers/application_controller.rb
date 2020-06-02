fcd9cb1f9b953c271ec6c8e9ab952e88700195ad
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-06-03 09:24:30 +1200

    Added first name and last name to login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :user_name, :phone_number, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     # Code from lecture: devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :user_name, :phone_number, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :avatar])
 
   end
 end
