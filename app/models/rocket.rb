187e6b6a86cd1b9e12b7efca78803d735fc3ac5f
Rithvik Chand <rithvikchand@gmail.com>
2020-06-03 14:30:50 +1000

    mapbox stuffs

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,11 +1,13 @@
 class Rocket < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   has_many_attached :rocket_images
   belongs_to :user
   has_many :orders
   has_many :reviews, through: :orders
   validates :name, :payload, :price, :description, :location, presence: true
   validates :name, uniqueness: true
-  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1_000_000 }
 
   def owner
     # checks who the owner is
